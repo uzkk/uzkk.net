25fd642227b51b8b330bf24f6c076009fa64b874
葒茶菌 <kouchyakun@gmail.com>
2019-03-22 14:11:32 +0800

    add a ranknum radio

--- docs/.vuepress/favorite/Settings.vue
+++ docs/.vuepress/favorite/Settings.vue
@@ -1,5 +1,29 @@
 <template>
   <div>
     <div class="title">本命测试</div>
+    <span class="ranknum">
+      <span class="rank-item" v-for="num in ranks">
+        <input type="radio" v-model="ranknum">
+        <label>{{num}}</label>
+      </span>
+    </span>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Settings',
+  data () {
+    return {
+      ranks: [1, 5, 7, 10, 20],
+      ranknum: 1
+    }
+  },
+  methods: {},
+  props: []
+}
+</script>
+
+<style scoped>
+
+</style>
