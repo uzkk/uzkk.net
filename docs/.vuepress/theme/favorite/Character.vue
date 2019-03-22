57ef07f8e3b81a422e7f49a8e1a1c431e9c14090
Shigma <1700011071@pku.edu.cn>
2019-03-22 23:28:20 +0800

    char view

---
+++ docs/.vuepress/theme/favorite/Character.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="char-view">
+    <img :src="src" :alt="node.name" @click.stop="$emit('click')">
+    <div class="info">
+      <div>{{ node.name }}</div>
+      <div>{{ node.nick }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: ['node', 'face'],
+
+  computed: {
+    src () {
+      return `${TH_CHAR_PATH}/${this.face}/${this.node.img}.png`
+    },
+  },
+}
+
+</script>
+
+<style lang="stylus" scoped>
+
+.char-view
+  position relative
+  border 1px solid black
+  box-shadow 0 2px 2px 1px #0008
+
+  img
+    display block
+    cursor pointer
+
+  .info
+    position absolute
+    width 100%
+    transition 0.3s ease
+    background #0006
+    color #fffb
+    bottom 0
+
+</style>
